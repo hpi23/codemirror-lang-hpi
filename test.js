27f07ab1d62b87781e9b33fc4d3075abf760b0dd
Mik Mueller <83001409+MikMuellerDev@users.noreply.github.com>
2023-09-25 21:23:01 +0200

    chore: Remove `test.js`

--- test.js
+++
@@ -1,8 +0,0 @@
-import {rushLanguage} from './dist/index.js'
-import {testTree} from "@lezer/generator/test"
-
-let ast = rushLanguage.parser.parse("let a = 1 + 2 - 1")
-let spec = `
-`
-
-testTree(ast, spec)
